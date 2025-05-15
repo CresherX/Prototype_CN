5e0bd964ccc691174f4e5f07d856bd28a4e17bea
CresherX <kirjll822@gmail.com>
2025-05-15 20:36:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from flask import Flask
 from app.db import init_db
 from app.route import routes
 from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+init_db()
 @app.route("/parse", methods=["GET"])
 def trigger_parse():
     from app.parser import parse_articles  
     parse_articles()  # вызывает insert_article
     return "Парсинг выполнен", 200
-app = Flask(__name__)
-CORS(app)
-
-init_db()
-
 # Регистрация Blueprint без префикса
 app.register_blueprint(routes, url_prefix="/api")
 if __name__ == "__main__":
