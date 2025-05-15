6d18e8b0310c7e4cbc2c6efb668fe5730e6d0ce6
CresherX <kirjll822@gmail.com>
2025-05-15 18:53:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
-load_dotenv()  # Загружаем переменные из .env файла
+# Загрузка .env только локально
+if os.getenv("RAILWAY_ENVIRONMENT") is None:
+    load_dotenv()
 
 DB_CONFIG = {
-    'host': os.getenv('DB_HOST'),
-    'user': os.getenv('DB_USER'),
-    'password': os.getenv('DB_PASSWORD'),
-    'database': os.getenv('DB_NAME'),
-    'port': int(os.getenv('DB_PORT', 3306))
+    "host": os.getenv("DB_HOST", "localhost"),
+    "port": int(os.getenv("DB_PORT", 3306)),
+    "user": os.getenv("DB_USER", "root"),
+    "password": os.getenv("DB_PASSWORD", ""),
+    "database": os.getenv("DB_NAME", "mydb"),
 }
