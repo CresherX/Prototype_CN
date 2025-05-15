616744b515adf4f95eefcc12e373c3cf83afb5fc
CresherX <kirjll822@gmail.com>
2025-05-15 22:06:30 +0300

    Update route.py

--- app/route.py
+++ app/route.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+from app.db import get_all_articles, get_articles_by_source, fetch_latest_articles_from_db  # убедись, что эти функции есть
 
 routes = Blueprint('routes', __name__)
 
@@ -12,7 +13,7 @@ def articles_by_source(source):
     articles = get_articles_by_source(source)
     return jsonify(articles), 200
 
-@routes.route("/articles/latest/<int:count>", methods=["GET"])
+@routes.route('/articles/latest/<int:count>', methods=['GET'])
 def get_latest_articles(count):
     articles = fetch_latest_articles_from_db(count)
     return jsonify(articles), 200
